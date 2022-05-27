fafb1784233d480771254cf7b8fca74415a32617
KajalTomar <kajaltomar09@gmail.com>
2022-05-26 22:01:11 -0500

    working for now

--- a1-elf.c
+++ a1-elf.c
@@ -22,24 +22,44 @@ typedef struct ELF_HEADER{
 	uint8_t target_OS; 
 	uint16_t obj_file_type;
 	uint16_t ISA;
-	uint64_t program_header_address;
-	uint64_t section_header_address;
+	
+	uint64_t entrypoint_address;
+	uint64_t p_header_address;
+	uint16_t p_header_size;
+	uint16_t p_header_amount;
+	
+	uint16_t s_header_size;
+	uint16_t s_header_amount;
+
+	uint16_t indexof_s_header;
 
 } elf_header;
 
+typedef struct PROGRAM_HEADER{
+	uint32_t segment_type;
+	uint64_t segment_address;
+	uint64_t segment_size;	
+} program_header;
+
+
 void read_header(elf_header * header, int handle);
 void print_header(elf_header * header); 
+void read_program_header(program_header * p_header, elf_header * elf_header, int handle); 
+void print_program_header(program_header * header); 
 
 int main(void){
 	
 	char *filename = "hello.out32";
 	elf_header header; 
-
+	program_header p_header;
 	
 	int elf_fd = open(filename, O_RDONLY);
 	
 	read_header(&header, elf_fd);
 	print_header(&header);
+
+	
+	read_program_header(&p_header, &header, elf_fd);
 
 	printf("End of process.\n");
 	
@@ -69,18 +89,27 @@ void read_header(elf_header * header, int handle){
 	lseek(handle, 4, SEEK_CUR);
 
 	if(header->format == 1){ // 32-bit
-		read(handle, &header->program_header_address, 4);
-		read(handle, &header->section_header_address, 4);
+		read(handle, &header->entrypoint_address, 4);
+		read(handle, &header->p_header_address, 4);
+		lseek(handle, 4, SEEK_CUR);
 	}
 	else if(header->format == 2){ // 64-bit
-		read(handle, &header->program_header_address, 8);
-		read(handle, &header->section_header_address, 8);
+		read(handle, &header->entrypoint_address, 8);
+		read(handle, &header->p_header_address, 8);
+		lseek(handle, 8, SEEK_CUR);
 	}
+
+	lseek(handle, 6, SEEK_CUR);
+	
+	read(handle, &header->p_header_size, 2);
+	read(handle, &header->p_header_amount, 2);
+ 	read(handle, &header->s_header_size, 2);
+  	read(handle, &header->s_header_amount, 2);
+	read(handle, &header->indexof_s_header, 2);
+
 }
 
-
 void print_header(elf_header * header){
-
 	assert(header != NULL);
 	
 	printf("ELF header:\n");
@@ -100,20 +129,81 @@ void print_header(elf_header * header){
 	}
 	
 	
-	printf("* compiled for 0x%02x (operating system)\n"
-			"* has type %#04x \n"
-			"* compiled for %#04x (isa) \n",
+	printf("* compiled for 0x%02x (operating system).\n"
+			"* has type %#04x. \n"
+			"* compiled for %#04x (isa). \n",
 			header->target_OS, header->obj_file_type, header->ISA);
 	
 	if(header->format == 1){ // 32-bit
-		printf("* entry point address 0x%08lx \n"
-			"* program header table starts at 0x%08lx \n",
-			header->program_header_address, header->section_header_address);
+		printf("* entry point address 0x%08lx. \n"
+			"* program header table starts at 0x%08lx. \n",
+			header->entrypoint_address, header->p_header_address);
 	}
 	else if(header->format == 2){ // 64-bit
-		printf("* entry point address  0x%016lx \n"
-				"* program header table starts at 0x%016lx \n",
-				header->program_header_address, header->section_header_address);
+		printf("* entry point address  0x%016lx. \n"
+				"* program header table starts at 0x%016lx. \n",
+				header->entrypoint_address, header->p_header_address);
 	}
+
+	printf("There are %i program headers, each is %i bytes. \n"
+			"There are %i section headers, each is %i bytes. \n"
+			"The section header string table is %i.\n",
+			header->p_header_amount, header->p_header_size, header->s_header_amount, header->s_header_size, header->indexof_s_header);
 }
+
+void read_program_header(program_header * p_header, elf_header * elf_header, int handle){
+		
+	assert(elf_header != NULL);
+	assert(p_header != NULL);
+	assert(handle >= 0);	
+		
+	
+	for(int i = 0; i < elf_header->p_header_amount; i++){
+	
+		lseek(handle, elf_header->p_header_address, SEEK_SET);
+		lseek(handle, i*elf_header->p_header_size, SEEK_CUR); 
+
+		read(handle, &p_header->segment_type, 4); 
+
+		if(elf_header->format == 2){ // 64-bit 
+			lseek(handle, 12, SEEK_CUR);
+			read(handle, &p_header->segment_address, 8);
+			lseek(handle, 8, SEEK_CUR);
+			read(handle, &p_header->segment_size, 8);
+		}
+	
+		if(elf_header->format == 1){ // 32-bit
+			lseek(handle,4, SEEK_CUR);
+			read(handle, &p_header->segment_address, 4);
+			lseek(handle, 4, SEEK_CUR);
+			read(handle, &p_header->segment_size, 4);
+		}
+			printf("\nProgram header #%i\n", i+1);
+			print_program_header(p_header);
+		}
+
+	}
+
+void print_program_header(program_header * header){
+	assert(header != NULL);
+	
+	printf("*segment type 0x%08ix.\n"
+			"*virtual address of segment 0x%08lx. \n"
+			"*size in file %lu bytes.\n",
+			header->segment_type, header->segment_address, header->segment_size);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- a1-procs.c
+++ a1-procs.c
@@ -1,4 +1,4 @@
-//-----------------------------------------
+//////-------------------------------------------
 ////// NAME: Kajal Tomar
 ////// STUDENT NUMBER: 7793306
 ////// COURSE: COMP 3430, SECTION: A01
@@ -11,11 +11,92 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int read_config();
+void create_workers(int amount); 
+
+int worker_amount;
+pid_t child; 
+pid_t parent_id; 
+
+void handle_SIGINT(int sig){
+	(void) sig;
+	
+	if(getppid() == parent_id){
+		printf("<%i>is being killed.\n", getpid());
+		printf("<%i>is being exiting.\n", getpid());
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void handle_SIGHUP(int sig){
+	(void) sig;
+
+	int new_workers = read_config() - worker_amount;
+		
+	if(new_workers > 0){
+		// need that many more
+		printf("<%d> Must create %i new workers.\n", getpid(), new_workers);
+		create_workers(new_workers);
+		exit(EXIT_SUCCESS);
+	}
+	else if(new_workers < 0){
+		// need that many less 
+	}
+
+	worker_amount = new_workers; 
+	
+}
 
 int main(void){
+	parent_id = getpid();	
+	worker_amount =	read_config();
+	
+	signal(SIGINT, handle_SIGINT);
+	signal(SIGHUP, handle_SIGHUP);
+	
+	printf("<%d>I am running\n", getpid());	
+			
+	create_workers(worker_amount); 
 
-	printf("End of process.\n");
+	if(child == 0){
+		printf("<%d> is starting\n", getpid());
+		fflush(stdout);
+		while(1){}
+	}
+	else{			
+		wait(NULL);			
+	//	printf("End of process.\n");
+	}
 	
 	return EXIT_SUCCESS;
+
+}
+
+int read_config(){
+	FILE *config_file  = fopen("config.txt", "r"); // read only 
+	int amount_read;
+	assert(config_file != NULL); 
+	printf("Reading config file.\n");
+	fscanf(config_file, "%i", &amount_read);
+	printf("Changing setting to: %i\n", amount_read);
+
+	fclose(config_file);
+	return amount_read;
+
+}
+
+void create_workers(int amount){
+	for(int i = 0; i < amount; i++){
+		if(getpid() == parent_id){
+			child = fork();
+		}
+		else{
+			break;								         
+		}
+	}
 
 }
