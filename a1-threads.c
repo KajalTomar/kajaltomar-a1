276ef1f05b1215ee78773f52dec961086b9f31be
Kajal Tomar <52635061+KajalTomar@users.noreply.github.com>
2024-07-05 15:31:37 -0500

    Cleaning up the comments

--- a1-threads.c
+++ a1-threads.c
@@ -1,14 +1,3 @@
-//-----------------------------------------
-////// NAME: Kajal Tomar
-////// STUDENT NUMBER: 7793306
-////// COURSE: COMP 3430, SECTION: A01
-////// INSTRUCTOR: Franklin Bristow
-////// ASSIGNMENT: assignment 1, QUESTION: question 3
-//////
-////// REMARKS: What is the purpose of this program?
-//////
-//////-----------------------------------------
-
 #include <stdio.h>
 #include <stdlib.h>
 
